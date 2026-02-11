9af3044ba253e3118f1b0384eda18c4688ebc2c0
chehek <chehek.khindria@gmail.com>
2026-02-11 17:19:53 +0530

    Fix: Added missing PostMapping import and fixed service path

--- src/main/java/com/expense/expense_tracker/WebController.java
+++ src/main/java/com/expense/expense_tracker/WebController.java
@@ -1,6 +1,9 @@
 package com.expense.expense_tracker;
 
+import com.expense.expense_tracker.model.Expense;
+import org.springframework.web.bind.annotation.PostMapping;
 import com.expense.expense_tracker.ExpenseService;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,5 +19,11 @@ public class WebController {
     public String viewHomePage(Model model) {
         model.addAttribute("listExpenses", service.getExpenses());
         return "index";
+    }
+
+    @PostMapping("/save")
+    public String saveExpense(@ModelAttribute("expense") Expense expense) {
+        service.saveExpense(expense);
+        return "redirect:/"; //tells browser to refresh the page
     }
 }
